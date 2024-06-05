4ca91a9239f07f83d95e71a028c8b5b5da33cde6
LekiaAnonim <lekiaprosper@gmail.com>
2024-06-05 14:37:11 +0100

    added production database

--- eureka/settings/base.py
+++ eureka/settings/base.py
@@ -116,7 +116,15 @@ WSGI_APPLICATION = "eureka.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#     }
+# }
+DATABASES = {
+    "default": dj_database_url.config(default='postgresql://postgres:qDeZYDXnjsGIKOhMQjofGqcPUwWPuiPS@monorail.proxy.rlwy.net:31039/railway', conn_max_age=1800),
+}
 
 
 
--- eureka/settings/production.py
+++ eureka/settings/production.py
@@ -11,9 +11,6 @@ env = environ.Env(
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 DEBUG = env('DEBUG')
 
-DATABASES = {
-    "default": dj_database_url.config(default='postgresql://postgres:qDeZYDXnjsGIKOhMQjofGqcPUwWPuiPS@monorail.proxy.rlwy.net:31039/railway', conn_max_age=1800),
-}
 
 try:
     from .local import *
