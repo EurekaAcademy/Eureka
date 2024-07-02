aad6b4fe1a635a25ac35e0590ca9b4309f717c46
LekiaAnonim <lekiaprosper@gmail.com>
2024-07-02 14:07:23 +0100

    run collectstatic to correct Internal Server error

--- eureka/settings/production.py
+++ eureka/settings/production.py
@@ -1,9 +1,7 @@
 from .base import *
 import environ
 
-DEBUG = False
 
-ALLOWED_HOSTS = ["eurekadataacademy.com", "eureka-production-6efb.up.railway.app"]
 
 env = environ.Env(
     DEBUG=(bool, False)
@@ -11,6 +9,7 @@ env = environ.Env(
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 DEBUG = env('DEBUG')
 
+ALLOWED_HOSTS = ["eurekadataacademy.com", "eureka-production-6efb.up.railway.app"]
 
 EMAIL_HOST = 'smtp.elasticemail.com'
 EMAIL_USE_TLS = True
